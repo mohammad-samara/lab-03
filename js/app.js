cb1de1f6aa4c5adb606b6608f34a9d452e923b51
mohammad samara <mohammad.ab.samara@gmail.com>
2020-07-21 19:30:45 +0300

    almost

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 let allImage = [];
-let allImage1=[];
-let allImage2=[];
-let nextPageImgCounter=0;
+let allImage1 = [];
+let allImage2 = [];
+let Rendered = [];
+let currentFilter="";
+let nextPageImgCounter = 0;
 //let keywords = [];
-let keywords1=[];
-let keywords2=[];
-let currentPage="page1";
-function findKeywords(item ,keywordsNum) {
+let keywords1 = [];
+let keywords2 = [];
+let currentPage = "page1";
+function findKeywords(item, keywordsNum) {
     if (keywordsNum.length == 0) { keywordsNum.push(item.keyword.toLowerCase()); }
     var matched = false;
     for (let i = 0; i < keywordsNum.length; i++) {
@@ -22,16 +24,16 @@ function ImgData(item, pageNum) {
     this.description = item.description;
     this.keyword = item.keyword.toLowerCase();
     this.horns = item.horns;
-    this.pageNum=pageNum;
+    this.pageNum = pageNum;
     allImage.push(this);
-    if(pageNum=="page1"){allImage1.push(this);}else{allImage2.push(this);}
+    if (pageNum == "page1") { allImage1.push(this); } else { allImage2.push(this); }
 }
 ImgData.prototype.render = function (pageNum) {
-    
-    let mustacheTemplate=$('#imgCard').html();
-    let newObject=Mustache.render(mustacheTemplate, this);
+
+    let mustacheTemplate = $('#imgCard').html();
+    let newObject = Mustache.render(mustacheTemplate, this);
     //newObject.addClass(pageNum);
-    $('main').append(newObject);
+    $('#rendering').append(newObject);
 }
 
 //page1 or default page
@@ -39,12 +41,14 @@ $.get('data/page-1.json').then(data => {
     data.forEach(item => {
         let imgData = new ImgData(item, "page1");
         //imgData.render();
-        findKeywords(item ,keywords1);
+        findKeywords(item, keywords1);
     });
-    for (let i=0; i<allImage.length;i++){
-        allImage[i].render("page1");
+    allImage1.sort(sortByTitle);
+    for (let i = 0; i < allImage1.length; i++) {
+        allImage1[i].render("page1");
         nextPageImgCounter++;
     }
+    Rendered = allImage1;
 
     ///console.log("lenght from inside : " + keywords.length);
     for (let i = 0; i < keywords1.length; i++) {                                  // add keywords as options in the selection form
@@ -58,12 +62,9 @@ $.get('data/page-2.json').then(data => {
     ///console.log("lenght from page2 : " + keywords.length);
     data.forEach(item => {
         let imgData = new ImgData(item, "page2");
-        findKeywords(item ,keywords2);
+        findKeywords(item, keywords2);
     });
-    for (let i=nextPageImgCounter; i<allImage.length;i++){
-        allImage[i].render("page2");
-        nextPageImgCounter++;
-    }
+    allImage2.sort(sortByTitle);
 
     // for (let i = 0; i < keywords2.length; i++) {                                  // add keywords as options in the selection form
     //     let keyOption = $(`<option value="${keywords2[i]}">${keywords2[i]}</option>`);
@@ -73,11 +74,15 @@ $.get('data/page-2.json').then(data => {
     $('.page2').hide();
 });
 //////page2 click
-$('#page2').on('click', function (event){
+$('#page2').on('click', function (event) {
     event.preventDefault();
-    currentPage="page2";
-    $('.page2').show();
-    $('.page1').hide();
+    currentPage = "page2";
+    $('#rendering').empty();
+    for (let i = 0; i < allImage2.length; i++) {
+        allImage2[i].render("page2");
+        nextPageImgCounter++;
+    }
+    Rendered = allImage2;
     $('#imgFilter').empty();
     $('#imgFilter').append('<option>Filter by Keyword</option>');
     for (let i = 0; i < keywords2.length; i++) {                                  // add keywords as options in the selection form
@@ -86,11 +91,15 @@ $('#page2').on('click', function (event){
     }
 });
 //////page1 click
-$('#page1').on('click', function (event){
+$('#page1').on('click', function (event) {
+    Rendered = allImage1;
     event.preventDefault();
-    currentPage="page1";
-    $('.page1').show();
-    $('.page2').hide();
+    currentPage = "page1";
+    $('#rendering').empty();
+    for (let i = 0; i < allImage1.length; i++) {
+        allImage1[i].render("page1");
+        nextPageImgCounter++;
+    }
     $('#imgFilter').empty();
     //let keyOption = $(`<option value="${keywords1[i]}">${keywords1[i]}</option>`);
     $('#imgFilter').append('<option>Filter by Keyword</option>');
@@ -102,9 +111,52 @@ $('#page1').on('click', function (event){
 //////////////// selection and filter
 $('#imgFilter').on('change', function (event) {
     var selected = this.options[this.selectedIndex].value;
-    console.log(selected);
+    currentFilter=selected;
     $('.imgInfo').hide();
     $(`.${selected}.${currentPage}`).show();
-    if (this.selectedIndex == 0) { $(`.${currentPage}`).show();}
+    if (this.selectedIndex == 0) { $(`.${currentPage}`).show(); }
 
 });
+
+////////sort
+$('#selectSort').on('change', function (event) {
+    //var selected = this.options[this.selectedIndex].value;
+    var selected = this.value;
+    if (selected == "titleSort") {
+        Rendered.sort(sortByTitle);
+
+        $('#rendering').empty();
+        for (let i = 0; i < Rendered.length; i++) {
+            Rendered[i].render(`${currentPage}`);
+        }
+
+        $('.imgInfo').hide();
+        console.log(currentFilter);
+        if(currentFilter==""){$(`.${currentPage}`).show();}else{
+            $(`.${currentFilter}.${currentPage}`).show();}
+    }
+    if (selected == "hornsSort") {
+        Rendered.sort(sortByHorns);
+
+        $('#rendering').empty();
+        for (let i = 0; i < Rendered.length; i++) {
+            Rendered[i].render(`${currentPage}`);
+        }
+        $('.imgInfo').hide();
+        if(currentFilter==""){$(`.${currentPage}`).show();}else{
+        $(`.${currentFilter}.${currentPage}`).show();}
+    }
+    // $('.imgInfo').hide();
+    // $(`.${selected}.${currentPage}`).show();
+    // if (this.selectedIndex == 0) { $(`.${currentPage}`).show();}
+
+});
+
+function sortByTitle(obj1, obj2) {
+    return obj1.title.localeCompare(obj2.title);
+
+}
+
+function sortByHorns(obj1, obj2) {
+    return obj1.horns - obj2.horns;
+}
